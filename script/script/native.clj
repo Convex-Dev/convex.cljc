cac4f07a7de2f466d9bc3818a8687f76579692c7
Adam Helinski <adam@helins.io>
2021-07-13 16:25:01 +0200

    Fix native compilation of watch mode in runner
    Previously, the runner was compiled to a native binary that did not
    support watch mode. Now it is supported after doing some work on
    reflection, meaning the native runner is on par with its JAR
    counterpart.

--- script/script/native.clj
+++ script/script/native.clj
@@ -4,11 +4,55 @@
 
   {:author "Adam Helinski"}
 
+  (:refer-clojure :exclude [agent])
   (:require [babashka.tasks    :as bb.task]
+            [cheshire.core     :as cheshire]
+            [clojure.java.io]
             [clojure.string]))
 
 
 ;;;;;;;;;;
+
+
+(defn agent
+  
+  "Starts the native-image-agent for tracing and output reflection configuration."
+
+  []
+
+  (apply bb.task/shell
+         "java"
+         "-agentlib:native-image-agent=config-output-dir=./private/agent"
+         *command-line-args*))
+
+
+
+(defn reflect-config
+
+  "Copies './private/agent/reflect-config.json' to the folder given as CLI arg after doing some preparation."
+
+  ;; Inspired by https://github.com/borkdude/refl/blob/4eefd89b1f70407ac3e50c84ce2a6ceae3babad3/script/gen-reflect-config.clj#L64-L67
+  ;;
+  ;; Without this, building the native image will fail.
+
+  []
+
+  (cheshire/generate-stream (mapv (fn [hmap]
+                                    (if (= (get hmap
+                                                "name")
+                                           "java.lang.reflect.Method")
+                                      (do (println :got hmap)
+                                      {"name"    "java.lang.reflect.AccessibleObject"
+                                       "methods" [{"name" "canAccess"
+                                                   "parameterTypes" ["java.lang.Object"]}]})
+                                      hmap))
+                                  (cheshire/parse-stream (clojure.java.io/reader "./private/agent/reflect-config.json")))
+                            (clojure.java.io/writer (str (or (first *command-line-args*)
+                                                             (throw (ex-info "Path to project root missing"
+                                                                             {})))
+                                                         "reflect-config.json"))
+                            {:pretty true}))
+
 
 
 (defn image
@@ -27,6 +71,5 @@
            "--initialize-at-build-time"
            "--no-fallback"
            "--no-server"
-           "--initialize-at-run-time=com.barbarysoftware.jna.CarbonAPI"
            "-H:+ReportExceptionStackTraces"
            (rest *command-line-args*))))
