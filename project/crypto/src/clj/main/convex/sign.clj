d6bdf9aa2953c8d29cee117de6f791844c8ae56a
Adam Helinski <adam@helins.io>
2021-08-31 18:03:10 +0200

    Add creating and handling key pairs in `$.sign`

--- project/crypto/src/clj/main/convex/sign.clj
+++ project/crypto/src/clj/main/convex/sign.clj
@@ -5,13 +5,35 @@
   {:author "Adam Helinski"}
 
   (:import (convex.core.crypto AKeyPair
-                               Ed25519KeyPair)))
+                               Ed25519KeyPair)
+           (convex.core.data AccountKey
+                             ACell
+                             SignedData)
+           (java.security PrivateKey
+                          PublicKey)))
 
 
-;;;;;;;;;;
+(set! *warn-on-reflection*
+      true)
 
 
-(defn gen-ed25519
+;;;;;;;;;; Creating key pairs
+
+
+(defn ed25519-from
+
+  ""
+
+  ^AKeyPair
+
+  [^PublicKey key-public ^PrivateKey key-private]
+
+  (Ed25519KeyPair/create key-public
+                         key-private))
+
+
+
+(defn ed25519-gen
 
   ""
 
@@ -19,6 +41,57 @@
 
   []
 
-  
-  )
+  (Ed25519KeyPair/generate))
 
+
+;;;;;;;;;; Retrieving keys from key pairs
+
+
+(defn account-key
+
+  ""
+
+  ^AccountKey
+
+  [^AKeyPair key-pair]
+
+  (.getAccountKey key-pair))
+
+
+
+(defn key-private
+
+  ""
+
+  ^PrivateKey
+
+  [^AKeyPair key-pair]
+
+  (.getPrivate key-pair))
+
+
+
+(defn key-public
+
+  ""
+
+  ^PublicKey
+
+  [^AKeyPair key-pair]
+
+  (.getPublic key-pair))
+
+
+;;;;;;;;;; Using key pairs
+
+
+(defn sign
+
+  ""
+
+  ^SignedData
+
+  [^AKeyPair key-pair ^ACell cell]
+
+  (.signData key-pair
+              cell))
