82f8764b26611255f568b4b6137f2f672f643bac
Adam Helinski <adam@helins.io>
2021-09-29 22:44:14 +0200

    Add data structure std functions

--- project/cvm/src/clj/main/convex/std.clj
+++ project/cvm/src/clj/main/convex/std.clj
@@ -4,14 +4,24 @@
 
   {:author "Adam Helinski"}
 
-  (:import (convex.core.data ACountable
+  (:import (convex.core.data ACell
+                             ACountable
                              ADataStructure))
-  (:refer-clojure :exclude [count
+  (:refer-clojure :exclude [assoc
+                            conj
+                            contains?
+                            count
+                            empty
                             empty?
+                            get
                             nth]))
 
 
-;;;;;;;;;;
+(set! *warn-on-reflection*
+      true)
+
+
+;;;;;;;;;; Countable
 
 
 (defn count
@@ -55,3 +65,67 @@
 
   (.getElementRef countable
                   index))
+
+
+;;;;;;;;;; Data structures
+
+
+(defn assoc
+
+  ""
+
+  [^ADataStructure coll k v]
+
+  (.assoc coll
+          k
+          v))
+
+
+
+(defn conj
+
+  ""
+
+  [^ADataStructure coll v]
+
+  (.conj coll
+         v))
+
+
+
+(defn contains?
+
+  ""
+
+  [^ADataStructure coll k]
+
+  (.containsKey coll
+                k))
+
+
+
+(defn empty
+
+  ""
+
+  [^ADataStructure coll]
+
+  (.empty coll))
+
+
+
+(defn get
+
+  ""
+
+  ([^ADataStructure coll ^ACell k]
+
+   (.get coll
+         k))
+
+
+  ([^ADataStructure coll ^ACell k not-found]
+
+   (.get coll
+         k
+         not-found)))
--- project/cvm/src/clj/test/convex/test/std.clj
+++ project/cvm/src/clj/test/convex/test/std.clj
@@ -4,17 +4,21 @@
 
   {:author "Adam Helinski"}
 
-  (:refer-clojure :exclude [count
+  (:refer-clojure :exclude [assoc
+                            conj
+                            contains?
+                            count
+                            empty
                             empty?
+                            get
                             nth])
   (:require [clojure.test :as T]
             [convex.cell  :as $.cell]
-            [convex.read  :as $.read]
             [convex.ref   :as $.ref]
             [convex.std   :as $.std]))
 
 
-;;;;;;;;;;
+;;;;;;;;;; Countable
 
 
 (T/deftest count
@@ -131,3 +135,139 @@
              (-> ($.std/nth-ref ($.cell/blob (byte-array [1 2]))
                                 0)
                  $.ref/resolve))))))
+
+
+;;;;;;;;;; Data structure
+
+
+(T/deftest assoc
+
+  (T/is (= ($.cell/* (:b))
+           ($.std/assoc ($.cell/* (:a))
+                        ($.cell/* 0)
+                        ($.cell/* :b))))
+
+  (T/is (= ($.cell/* [:b])
+           ($.std/assoc ($.cell/* [:a])
+                        ($.cell/* 0)
+                        ($.cell/* :b))))
+
+  (T/is (= ($.cell/* {:a :c})
+           ($.std/assoc ($.cell/* {:a :b})
+                        ($.cell/* :a)
+                        ($.cell/* :c))))
+
+  (T/is (= ($.cell/* #{:a :b})
+           ($.std/assoc ($.cell/* #{:a})
+                        ($.cell/* :b)
+                        ($.cell/* true))))
+
+  (T/is (= ($.cell/* #{:a})
+           ($.std/assoc ($.cell/* #{:a :b})
+                        ($.cell/* :b)
+                        ($.cell/* false)))))
+
+
+
+(T/deftest conj
+
+  (T/is (= ($.cell/* (:b :a))
+           ($.std/conj ($.cell/* (:a))
+                       ($.cell/* :b))))
+
+  (T/is (= ($.cell/* [:a :b])
+           ($.std/conj ($.cell/* [:a])
+                       ($.cell/* :b))))
+
+  (T/is (= ($.cell/* {:a :b
+                      :c :d})
+           ($.std/conj ($.cell/* {:a :b})
+                       ($.cell/* [:c :d]))))
+  (T/is (= ($.cell/* #{:a :b})
+           ($.std/conj ($.cell/* #{:a})
+                       ($.cell/* :b)))))
+
+
+
+(T/deftest contains?
+
+  (T/is ($.std/contains? ($.cell/* (:a))
+                         ($.cell/* 0)))
+
+  (T/is (not ($.std/contains? ($.cell/* (:a))
+                              ($.cell/* 1))))
+
+  (T/is ($.std/contains? ($.cell/* [:a])
+                         ($.cell/* 0)))
+
+  (T/is (not ($.std/contains? ($.cell/* [:a])
+                              ($.cell/* 1))))
+
+  (T/is ($.std/contains? ($.cell/* {:a :b})
+                         ($.cell/* :a)))
+
+  (T/is (not ($.std/contains? ($.cell/* {:a :b})
+                              ($.cell/* :c))))
+
+  (T/is ($.std/contains? ($.cell/* #{:a})
+                         ($.cell/* :a)))
+
+  (T/is (not ($.std/contains? ($.cell/* #{:a})
+                              ($.cell/* :b)))))
+
+
+
+(T/deftest empty
+
+  (T/is (= ($.cell/* ())
+           ($.std/empty ($.cell/* (:a)))))
+
+  (T/is (= ($.cell/* [])
+           ($.std/empty ($.cell/* [:a]))))
+
+  (T/is (= ($.cell/* {})
+           ($.std/empty ($.cell/* {:a :b}))))
+
+  (T/is (= ($.cell/* #{})
+           ($.std/empty ($.cell/* #{:a})))))
+
+
+
+(let [a ($.cell/* :a)
+      b ($.cell/* :b)]
+
+  (T/deftest get
+  
+    (T/is (= a
+             ($.std/get ($.cell/* (:a))
+                        ($.cell/* 0))))
+
+    (T/is (= a
+             ($.std/get ($.cell/* [:a])
+                        ($.cell/* 0))))
+
+    (T/is (= b
+             ($.std/get ($.cell/* {:a :b})
+                        a)))
+
+    (T/is (= nil
+             ($.std/get ($.cell/* {:a :b})
+                        ($.cell/* :c))))
+
+    (T/is (= ($.cell/* :not-found)
+             ($.std/get ($.cell/* {:a :b})
+                        ($.cell/* :c)
+                        ($.cell/* :not-found))))
+
+    (T/is (= ($.cell/* true)
+             ($.std/get ($.cell/* #{:a})
+                        a)))
+
+    (T/is (= ($.cell/* false)
+             ($.std/get ($.cell/* #{:a})
+                        b)))
+
+    (T/is (= ($.cell/* :not-found)
+             ($.std/get ($.cell/* #{:a})
+                        b
+                        ($.cell/* :not-found))))))
