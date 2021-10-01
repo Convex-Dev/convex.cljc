bc5b137ca5b7810b67d93512a6581d3becb948e2
Adam Helinski <adam@helins.io>
2021-10-01 18:08:56 +0200

    Initialize `:project/gen`
    Which will host `test.check` generators for cells. In the longer term,
    it will render `:project/clojurify` obsolete.

---
+++ project/gen/src/clj/dev/convex/gen/dev_templ.clj
@@ -0,0 +1,20 @@
+(ns convex.gen.dev
+
+  "Daydreaming at the REPL."
+
+  {:clj-kondo/config '{:linters {:unused-import    {:level :off}
+                                 :unused-namespace {:level :off}}}}
+
+  (:require [convex.cell                   :as $.cell]
+            [convex.gen                    :as $.gen]
+            [clojure.test.check.generators :as TC.gen]))
+
+
+;;;;;;;;;;
+
+
+(comment
+
+
+
+  )
---
+++ project/gen/src/clj/main/convex/gen.clj
@@ -0,0 +1,14 @@
+(ns convex.gen
+
+  ""
+
+  {:author "Adam Helinski"}
+
+  (:require [convex.cell                   :as $.cell]
+            [clojure.test.check.generators :as TC.gen]))
+
+
+;;;;;;;;;;
+
+
+
