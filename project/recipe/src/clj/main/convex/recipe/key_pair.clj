355153f9d714c38c157c60cee0b5119f96eaa17d
Adam Helinski <adam@helins.io>
2021-10-03 17:29:10 +0200

    Provide more explanations in key pair recipe

--- project/recipe/src/clj/main/convex/recipe/key_pair.clj
+++ project/recipe/src/clj/main/convex/recipe/key_pair.clj
@@ -13,7 +13,7 @@
 
    Creating and handling PFX iles is done using namespace `convex.pfx`.
 
-   API: https://cljdoc.org/d/world.convex/crypto.clj/0.0.0-alpha0/api/convex"
+   More information about public-key cryptography: https://en.wikipedia.org/wiki/Public-key_cryptography"
 
   {:author "Adam Helinski"}
 
@@ -39,13 +39,13 @@
     (try
 
       (-> ($.pfx/load file-key-store)
-          ($.pfx/key-pair-get "my-peer"
+          ($.pfx/key-pair-get "my-key-pair"
                               "my-password"))
 
       (catch Throwable _ex
         (let [key-pair ($.sign/ed25519)]
           (-> ($.pfx/create file-key-store)
-              ($.pfx/key-pair-set "my-peer"
+              ($.pfx/key-pair-set "my-key-pair"
                                   key-pair
                                   "my-password")
               ($.pfx/save file-key-store))
@@ -58,13 +58,63 @@
 (comment
 
   
+  ;; Example directory where key pair will be stored.
+  ;;
+  (def dir
+       "private/recipe/key-pair/")
+
+  
   ;; The first time, the key pair is generated and stored in a PFX file.
   ;;
   ;; Each subsequent time, the key pair is always retrieved from that file.
   ;;
   ;; Deleting that 'keystore.pfx' file in this directory will lose it forever.
   ;;
-  (retrieve "private/recipe/key-pair")
+  (retrieve dir)
+
+
+  ;;
+  ;; Let us inspect the core ideas in `retrieve`.
+  ;;
+
+
+  ;; Generating a key pair.
+  ;;
+  (def key-pair
+       ($.sign/ed25519))
+
+
+  ;; File for storing our "key store" capable of securely hosting one or several key pairs.
+  ;;
+  (def file
+       (str dir
+            "/keystore.pfx"))
+
+
+  ;; We create a file for our "key store" capable of securely hosting one or several key pairs,
+  ;; our key pair under an alias and protected by a password. Lastly, the updated key store is
+  ;; saved to the file.
+  ;;
+  (-> ($.pfx/create file)
+      ($.pfx/key-pair-set "my-key-pair"
+                          key-pair
+                          "my-password")
+      ($.pfx/save file))
+
+
+  ;; At any later time, we can load that file and retrieve our key pair by providing its alias and
+  ;; its password.
+  ;;
+  (= key-pair
+     (-> ($.pfx/load file)
+         ($.pfx/key-pair-get "my-key-pair"
+                             "my-password")))
+
+
+  ;;
+  ;; Although not mandatory, it is a good idea also specifying a password for the key store itself
+  ;; when using `$.pfx/create`, `$.pfx/save`, and `$.pfx/load`.
+  ;;  
 
 
   )
