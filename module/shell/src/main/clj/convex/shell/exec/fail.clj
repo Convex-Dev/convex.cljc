49b5fac8abe491a3c353a86c62f46ae264cac86f
Adam Helins <adam@protosens.com>
2022-09-10 12:46:11 +0200

    Add secret SREQ for rethrow an exception map
    Since it can be tricky to use, let's keep it secret for now as opposed
    to having a public function dedicated to it.

--- module/shell/src/main/clj/convex/shell/exec/fail.clj
+++ module/shell/src/main/clj/convex/shell/exec/fail.clj
@@ -13,39 +13,51 @@
             [convex.std       :as $.std]))
 
 
+(declare rethrow)
+
+
 ;;;;;;;;;; Notifying a failure or full halt
+
+
+(defn err
+
+  "Must be called in case of failure related to executing CVX Lisp, `err` being an error map (see the [[convex.shell.err]]
+   namespace).
+  
+   Under CVX `$.catch/*stack*` in the context is a stack of error handling transactions. This functions pops
+   the next error handling transaction and prepends it to CVX `$.trx/*list*`, the list of transactions pending
+   for execution.
+
+   Also, error becomes available under `$/*result*`.
+
+   This simple scheme allows sophisticated exception handling to be implemented from CVX Lisp, as seen in the
+   `$.catch` acccount."
+
+  [env err]
+
+  (-> env
+      (update :convex.shell/ctx
+              $.cvm/exception-clear)
+      (rethrow ($.std/assoc err
+                            $.shell.kw/exception?
+                            ($.cell/* true)))))
+
 
 
 (let [trx-pop ($.cell/* ($.catch/pop))]
 
-  (defn err
+  (defn rethrow
 
-    "Must be called in case of failure related to executing CVX Lisp, `err` being an error map (see the [[convex.shell.err]]
-     namespace).
-    
-     Under CVX `$.catch/*stack*` in the context is a stack of error handling transactions. This functions pops
-     the next error handling transaction and prepends it to CVX `$.trx/*list*`, the list of transactions pending
-     for execution.
+    "Like [[err]] but assumes the error has already been prepared as an exception result to return and the exception
+     on the CVM context has already been cleared."
 
-     Also, error becomes available under `$/*result*`.
+    [env ex]
 
-     This simple scheme allows sophisticated exception handling to be implemented from CVX Lisp, as seen in the
-     `$.catch` acccount."
-
-    [env err]
-
-    (let [err-2 ($.std/assoc err
-                             $.shell.kw/exception?
-                             ($.cell/* true))]
-      (-> env
-          (assoc :convex.shell/error
-                 err-2)
-          (cond->
-            (env :convex.shell/ctx)
-            (-> (update :convex.shell/ctx
-                        $.cvm/exception-clear)
-                ($.shell.ctx/def-result err-2)))
-          ($.shell.ctx/prepend-trx trx-pop)))))
+    (-> env
+        (assoc :convex.shell/error
+               ex)
+        ($.shell.ctx/prepend-trx trx-pop)
+        ($.shell.ctx/def-result ex))))
 
 
 
