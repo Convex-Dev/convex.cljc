11f796060be49bc70dfd3cf1984ded541161dee9
Adam Helins <adam@protosens.com>
2022-09-17 18:07:13 +0200

    Make Ed25519 BouncyCastle benchmark more pessimistic
    By not reusing the "signer". This is more realistic but actually does
    not have any impact on benchmarks.

--- module/bench/src/main/clj/convex/bench/ed25519/bouncycastle.clj
+++ module/bench/src/main/clj/convex/bench/ed25519/bouncycastle.clj
@@ -19,28 +19,29 @@
 
   (let [key-private (Ed25519PrivateKeyParameters. (byte-array Ed25519PrivateKeyParameters/KEY_SIZE))
         key-public  (.generatePublicKey key-private)
-        signer      (Ed25519Signer.)
         sign        (fn sign [^bytes payload]
-                      (.reset signer)
-                      (.init signer
-                             true
-                             key-private)
-                      (.update signer
-                               payload
-                               0
-                               (count payload))
-                      (.generateSignature signer))
+                      (let [signer (Ed25519Signer.)]
+                        (.reset signer)
+                        (.init signer
+                               true
+                               key-private)
+                        (.update signer
+                                 payload
+                                 0
+                                 (count payload))
+                        (.generateSignature signer)))
         verify      (fn verify [^bytes payload ^bytes signature]
-                      (.reset signer)
-                      (.init signer
-                             false
-                             key-public)
-                      (.update signer
-                               payload
-                               0
-                               (count payload))
-                      (.verifySignature signer
-                                        signature))]
+                      (let [signer (Ed25519Signer.)]
+                        (.reset signer)
+                        (.init signer
+                               false
+                               key-public)
+                        (.update signer
+                                 payload
+                                 0
+                                 (count payload))
+                        (.verifySignature signer
+                                          signature)))]
     [sign
      verify
      (vec (.getEncoded key-public))]))
