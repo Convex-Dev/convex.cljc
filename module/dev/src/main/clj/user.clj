7ad19a3b2f8757bc9ab25ec0cfbbb7705d86d44a
Adam Helins <adam@protosens.com>
2022-10-10 22:17:57 +0200

    Upgrade to latest Maestro version

--- module/dev/src/main/clj/user.clj
+++ module/dev/src/main/clj/user.clj
@@ -2,9 +2,9 @@
 
   "Does a bit of setup when starting."
 
-  (:require [clojure.string         :as string]
-            [portal.api             :as portal]
-            [protosens.maestro.user :as maestro.user]))
+  (:require [portal.api          :as portal]
+            [protosens.namespace :as P.namespace]
+            [protosens.symbol    :as P.symbol]))
 
 
 ;;;;;;;;;;
@@ -30,20 +30,16 @@
 
   "Require all namespaces from this repository that are present on the classpath."
 
-  []
+  []   
 
-  (maestro.user/require-filtered {:map-namespace  (fn [nmspace]
-                                                    (when (string/includes? (str nmspace)
-                                                                            "convex")
-                                                      [nmspace
-                                                       :as
-                                                       (symbol (str "$."
-                                                                    (second (string/split (str nmspace)
-                                                                                          #"convex\."))))]))
-                                  :require.before (fn [nmspace]
-                                                    (println "Require"
-                                                             nmspace))}))
-
+  (P.namespace/require-cp-dir+ (fn [nmspace]
+                                 (when (P.symbol/starts-with? nmspace
+                                                              'convex.)
+                                   [nmspace
+                                    :as
+                                    (P.symbol/replace-first nmspace
+                                                            'convex
+                                                            '$)]))))
 
 
 (def required-ns+
